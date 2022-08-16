d286045ed0b368c207bbfa766f55e893c2567026
dimainnature <dimainnature@yandex.ru>
2022-08-16 07:36:36 +0400

    Feat: Create base infrastructure.

---
+++ JwtExample.Gateway.Desktop/global.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Hosting;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.OpenApi.Models;
