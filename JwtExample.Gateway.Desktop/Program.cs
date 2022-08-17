d640bb8b3d454847eb89b6914364194bec2b8a77
dimainnature <dimainnature@yandex.ru>
2022-08-17 06:57:27 +0400

    Feat: Implement gateway for desktop consumers.

--- JwtExample.Gateway.Desktop/Program.cs
+++ JwtExample.Gateway.Desktop/Program.cs
@@ -2,15 +2,17 @@ namespace JwtExample.Gateway.Desktop;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    public static void Main(string[] args) =>
+        CreateHostBuilder(args)
+        .Build()
+        .Run();
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureAppConfiguration(configureDelegate:
+                   config => config.AddJsonFile(path: $"ocelot.json"));
             });
 }
--- JwtExample.Gateway.Desktop/global.cs
+++ JwtExample.Gateway.Desktop/global.cs
@@ -3,4 +3,6 @@ global using Microsoft.AspNetCore.Hosting;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
-global using Microsoft.OpenApi.Models;
+global using Ocelot.Cache.CacheManager;
+global using Ocelot.DependencyInjection;
+global using Ocelot.Middleware;
